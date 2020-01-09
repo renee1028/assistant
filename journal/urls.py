0dea109b02f25cf3ae3b1836d773773cf5450f29
renee1028 <reneesister@gmail.com>
2020-01-09 10:50:11 +0800

    數位助理

--- journal/urls.py
+++ journal/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import JournalList, JournalCreate, JournalUpdate, JournalDelete
 
 urlpatterns = [
-    path('', JournalList.as_view()), 
-    path('create/', JournalCreate.as_view()),
-    path('<int:pk>/update/', JournalUpdate.as_view()), 
-    path('<int:pk>/delete/', JournalDelete.as_view()),
+    path('', JournalList.as_view(),name='journal_list'), 
+    path('create/', JournalCreate.as_view(),name='journal_create'),
+    path('<int:pk>/update/', JournalUpdate.as_view(),name='journal_update'), 
+    path('<int:pk>/delete/', JournalDelete.as_view(),name='journal_delete'),
 ]
